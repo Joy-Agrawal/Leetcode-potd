3006e84fcb89b5d03b91de3cdf50e0946dea7662
Joy Agrawal <104222673+Joy-Agrawal@users.noreply.github.com>
2024-05-27 17:41:22 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 1608-special-array-with-x-elements-greater-than-or-equal-x/1608-special-array-with-x-elements-greater-than-or-equal-x.java
@@ -0,0 +1,34 @@
+class Solution {
+    public int specialArray(int[] nums) {
+        int n = nums.length;
+        if(n == 0){
+            return 0;
+        }
+        Arrays.sort(nums);
+        for(int i = 1;i<=n;i++){
+            int lb = findlb(nums,i);
+            if(n-lb == i){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private int findlb(int nums[],int x){
+        int st = 0,end = nums.length-1;
+        // int mid = st + (end-st)/2;
+        while(st<=end){
+            int mid = st + (end-st)/2;
+            if(nums[mid]==x){
+                return mid;
+            }
+            else if(nums[mid]>x){
+                end = mid-1;
+            }
+            else{
+                st = mid+1;
+            }
+        }
+        return st;
+    }
+}
