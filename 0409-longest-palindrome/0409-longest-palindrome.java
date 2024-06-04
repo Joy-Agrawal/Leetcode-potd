08c62fb2607587bf7cc9e5e1c4ea364466e4f53d
Joy Agrawal <104222673+Joy-Agrawal@users.noreply.github.com>
2024-06-04 10:12:49 +0530

    Time: 2 ms (86.01%), Space: 41 MB (98.85%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.java
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -4,7 +4,7 @@ class Solution {
         int n = s.length();
         for(int i = 0;i<n;i++){
             char ch = s.charAt(i);
-            if(ch<='z'){
+            if(ch>='a' && ch<='z'){
                 freq[ch-'a']++;
             }
             else{
