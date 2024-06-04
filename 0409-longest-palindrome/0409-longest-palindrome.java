d4a8cdcb3ad0539a1c5f6faa92fc12fa897658af
Joy Agrawal <104222673+Joy-Agrawal@users.noreply.github.com>
2024-06-04 10:12:43 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0409-longest-palindrome/0409-longest-palindrome.java
@@ -0,0 +1,37 @@
+class Solution {
+    public int longestPalindrome(String s) {
+        int freq[] = new int[52];
+        int n = s.length();
+        for(int i = 0;i<n;i++){
+            char ch = s.charAt(i);
+            if(ch<='z'){
+                freq[ch-'a']++;
+            }
+            else{
+                freq[ch-'A'+26]++;
+            }
+        }
+        int ans = 0;
+        int flag = 0;
+        for(int i = 0;i<52;i++){
+            if((freq[i]&1) == 0){
+                ans += freq[i];
+                freq[i] = 0;
+            }
+            else{
+                
+                if(freq[i]>2){
+                    ans += (freq[i]-1);
+                    freq[i] = 1;
+                }
+            }
+        }
+        for(int i = 0;i<52;i++){
+            if(freq[i] == 1){
+                flag = 1;
+                break;
+            }
+        }
+        return flag == 1? ans+1:ans;
+    }
+}
